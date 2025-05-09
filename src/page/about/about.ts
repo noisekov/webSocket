585ab8588a44e7cb594cd78d70f98673bc74aabe
noisekov <noise123q@mail.ru>
2025-05-09 22:43:16 +0300

    feat: create about page

--- src/page/about/about.ts
+++ src/page/about/about.ts
@@ -1,4 +1,5 @@
 import ButtonComponent from '../../components/button';
+import Component from '../../components/component';
 
 export default class AboutPage {
     constructor() {
@@ -10,9 +11,25 @@ export default class AboutPage {
     }
 
     render() {
-        return new ButtonComponent({
-            className: 'btn',
-            text: 'ABOUT',
-        }).getNode();
+        const content = new Component({
+            className: 'about-wrapper',
+        });
+        content.appendChildren([
+            new Component({
+                className: 'about-title',
+                text: 'Funny Chay',
+            }),
+            new Component({
+                className: 'about-subtitle',
+                text: 'The application is created using a third-party server and websokets',
+            }),
+            new ButtonComponent({
+                className: 'about-btn button',
+                text: 'Back',
+                onClick: () => window.history.go(-1),
+            }),
+        ]);
+
+        return content.getNode();
     }
 }
