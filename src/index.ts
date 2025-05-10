24c2fd709e23e93d214b2875115d5691e04f75e4
noisekov <noise123q@mail.ru>
2025-05-10 23:11:12 +0300

    feat: remove websoket ffrom index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import './index.scss';
 import ContentRender from './utils/ContentRender';
-const socket = new WebSocket('ws://localhost:4000');
-console.log(socket);
 
 new ContentRender().render();
