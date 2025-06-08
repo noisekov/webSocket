f987ea310577d3a1c46468955c6eef90073e4c64
noisekov <noise123q@mail.ru>
2025-06-08 16:01:26 +0300

    feat: add style for burger

--- src/components/BurgerMenu/BurgerMenu.ts
+++ src/components/BurgerMenu/BurgerMenu.ts
@@ -2,26 +2,14 @@ import Component from '../Component';
 
 export class BurgerMenu extends Component {
     constructor() {
-        super({});
+        super({ className: 'burger' });
         this.addComponents();
     }
 
     addComponents() {
-        const content = new Component({
-            tag: 'div',
-            className: 'burger-wrapper',
-        });
-        const burger = new Component({
-            tag: 'div',
-            className: 'burger',
-        });
-        burger.appendChildren([
+        this.appendChildren([
             new Component({ tag: 'span', className: 'burger-line' }),
         ]);
-        content.appendChildren([
-            new Component({ tag: 'input', className: 'search' }),
-            burger,
-        ]);
-        this.appendChildren([content]);
+        this.addListener('click', () => this.toggleClass('active'));
     }
 }
--- src/page/MainPage/MainPage.ts
+++ src/page/MainPage/MainPage.ts
@@ -2,17 +2,41 @@ import { BurgerMenu } from '../../components/BurgerMenu/BurgerMenu';
 import Component from '../../components/Component';
 
 export default class MainPage {
+    users: Component;
+    constructor() {
+        this.users = new Component({
+            tag: 'div',
+            className: 'users',
+        });
+    }
+
     render() {
         const content = new Component({
             className: 'main-wrapper',
         });
+        const chatWrapper = new Component({
+            className: 'main-chat',
+        });
+        const leftSideChat = new Component({
+            className: 'left-side',
+            tag: 'div',
+        });
+        const rightSideChat = new Component({
+            className: 'right-side',
+            tag: 'div',
+        });
+        leftSideChat.appendChildren([
+            new Component({ tag: 'input', className: 'search' }),
+            this.users,
+        ]);
+        chatWrapper.appendChildren([leftSideChat, rightSideChat]);
         content.appendChildren([
             new Component({
                 className: 'main-title title',
                 text: 'FUN CHAT',
             }),
-
             new BurgerMenu(),
+            chatWrapper,
         ]);
 
         return content.getNode();
