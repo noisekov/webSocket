9b83d551946e92687fb116d0e9b89183cf0422a9
noisekov <noise123q@mail.ru>
2025-06-12 22:46:46 +0300

    feat: add debounce

--- src/components/SearchInput/SearchInput.ts
+++ src/components/SearchInput/SearchInput.ts
@@ -14,37 +14,50 @@ export class SearchInput extends Component {
         this.addHandler();
     }
 
+    debounce<T>(func: (arg: T) => unknown, time: number) {
+        let timeout: NodeJS.Timeout;
+
+        return function (this: unknown, arg: T) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, [arg]), time);
+        };
+    }
+
     addHandler() {
         let isSetOldUsers = false;
 
-        this.addListener('input', (event) => {
-            const state = this.appState.getState();
-            const arrUsersBeforeFilter = state.users_before_filter;
-            const arrUsers = state.users_active.payload.users;
-            const filtredUsers = arrUsersBeforeFilter.length
-                ? arrUsersBeforeFilter
-                : arrUsers;
-            const inputValue = (event?.target as HTMLInputElement).value;
-            const filteredUsers = filtredUsers.filter(
-                (user: { login: string }) => {
-                    return user.login
-                        .toLowerCase()
-                        .includes(inputValue.toLowerCase());
-                }
-            );
+        this.addListener(
+            'input',
+            this.debounce((event) => {
+                const state = this.appState.getState();
+                const arrUsersBeforeFilter = state.users_before_filter;
+                const arrUsers = state.users_active.payload.users;
+                const filtredUsers = arrUsersBeforeFilter.length
+                    ? arrUsersBeforeFilter
+                    : arrUsers;
+                const inputValue = (event?.target as HTMLInputElement).value;
 
-            this.appState.setState({
-                users_active: {
-                    payload: {
-                        users: inputValue
-                            ? filteredUsers
-                            : state.users_before_filter,
+                const filteredUsers = filtredUsers.filter(
+                    (user: { login: string }) => {
+                        return user.login
+                            .toLowerCase()
+                            .includes(inputValue.toLowerCase());
+                    }
+                );
+
+                this.appState.setState({
+                    users_active: {
+                        payload: {
+                            users: inputValue
+                                ? filteredUsers
+                                : state.users_before_filter,
+                        },
                     },
-                },
-                ...(isSetOldUsers ? {} : { users_before_filter: arrUsers }),
-            });
+                    ...(isSetOldUsers ? {} : { users_before_filter: arrUsers }),
+                });
 
-            isSetOldUsers = true;
-        });
+                isSetOldUsers = true;
+            }, 500)
+        );
     }
 }
